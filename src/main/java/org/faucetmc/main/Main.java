4b85c2fe325a27dee9cabc7d943a318a9297f3cd
Asyc <aliheravi21@gmail.com>
2020-02-03 22:47:24 -0600

    Fixed Indents

--- src/main/java/org/faucetmc/main/Main.java
+++ src/main/java/org/faucetmc/main/Main.java
@@ -2,8 +2,8 @@ package org.faucetmc.main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
 }
